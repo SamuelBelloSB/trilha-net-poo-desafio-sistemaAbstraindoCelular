c13fe010aa6b5eb2cd7115657b36323293d71892
SamuelBelloSB <samuelbellocontato@gmail.com>
2024-09-02 20:19:10 -0300

    Implementação das propriedades Smartphone e do método abstrato do Iphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,35 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string? _modelo;
+        private string? _imei;
+        private int _memoria;
 
-        public Smartphone(string numero)
+        public string Modelo 
+        { 
+            get => _modelo;
+            set => _modelo = value != null ? value : throw new ArgumentNullException(nameof(value), "Houve uma exceção, o Modelo não pode ser nulo.");
+        }
+
+        public string Imei 
+        {
+            get => _imei;
+            set => _imei = value != null ? value : throw new ArgumentNullException(nameof(value), "Houve uma exceção, o IMEI não pode ser nulo.");
+        }
+
+        public int Memoria 
+        {
+            get => _memoria;
+            set => _memoria = value >= 0 ? value : throw new ArgumentNullException(nameof(value), "Houve uma exceção, a memória não pode ser 0.");
+        }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone1 = new Iphone("7777-7777", "Iphone 1", "IMEI-0001", 8);
+iphone1.InstalarAplicativo("Youtube");
